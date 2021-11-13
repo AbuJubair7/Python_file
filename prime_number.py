6c2937a1a9ffdbc63cb9a2adf2aa6194b772aa25
Asif Zaman <69311703+AsifZaman777@users.noreply.github.com>
2021-11-13 22:55:34 +0600

    LOC factor
    Minimize the code. rest you memory space

--- prime_number.py
+++ prime_number.py
@@ -1,21 +1,14 @@
-import math
+user_input=int(input("Input a number : "))
 
-number = int(input("Give a range: "))
-
-
-def is_prime(num):
-    for n in range(2, int(math.sqrt(num) + 1)):
-        if num % n == 0:
-            return False
-    return True
-
-
-total_prime = []
+def prime(num):
+    count=0
+    for i in range(2,num+1):
+        if num%i==0:
+            count++
+   return count
+if prime(user_input)==1:
+    print("Prime number")
+else:
+    print("Not a prime number")
  
-for i in range(1, number + 1):
-    if i != 1:
-        if is_prime(i):
-            print(i)
-            total_prime.append(i)
-
-print(f"Total prime : {len(total_prime)}")
+#Minimized LOC(Line of code)
